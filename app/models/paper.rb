12d75e1c165b060a1e8952d57feeb992078ddf18
Lukas Hagen <43916057+Greenscreen23@users.noreply.github.com>
2023-01-17 15:42:32 +0100

    Add filter

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,4 +1,6 @@
 class Paper < ApplicationRecord
+    scope :written_in, ->(year) { where("year = ?", year) if year.present? }
+
     has_and_belongs_to_many :authors
     validates :title, :venue, :year, presence: true
     validates :year, numericality: true
